f7256a1089a799d05ec47b07211974e6de303db3
Sridhar naik <sridharnaik24121999@gmail.com>
2022-08-22 14:12:55 +0530

    Inital version

---
+++ src/main/java/com/example/demo/impl/DefaultAddition.java
@@ -0,0 +1,13 @@
+package com.example.demo.impl;
+
+import com.example.demo.Addinterface;
+
+public class DefaultAddition implements Addinterface {
+
+	@Override
+	public int add(int n1, int n2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
